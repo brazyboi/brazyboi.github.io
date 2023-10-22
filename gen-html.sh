feeec824a0669b065b30421c18e9d4207cc905a8
Lucas Zhong <zhonglucas@gmail.com>
2023-10-21 23:42:44 -0700

    Changed gen-html.sh to create directories if they don't exist

--- gen-html.sh
+++ gen-html.sh
@@ -1,3 +1,7 @@
-find content -print | grep .md | while IFS= read -r MD_PATH; do
-	pandoc --template=page.template $MD_PATH > $(echo $MD_PATH | sed 's/content/docs/' - | sed 's/.md/.html/' -)
+find content -name '*.md' | while IFS= read -r md_path; do
+	build_path=$(echo $md_path | sed 's/content/build/' - | sed 's/\.md/\.html/' -)
+	folder_path="${build_path%/*}"
+	echo $build_path
+	mkdir -p $folder_path
+	pandoc --template=page.template $md_path -p > $build_path
 done
