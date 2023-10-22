c3eef9616134f08e72bb3351f92d7a6c7de2bbbe
Lucas Zhong <zhonglucas@gmail.com>
2023-10-21 23:45:28 -0700

    Add first post

--- gen-html.sh
+++ gen-html.sh
@@ -1,5 +1,5 @@
 find content -name '*.md' | while IFS= read -r md_path; do
-	build_path=$(echo $md_path | sed 's/content/build/' - | sed 's/\.md/\.html/' -)
+	build_path=$(echo $md_path | sed 's/content/docs/' - | sed 's/\.md/\.html/' -)
 	folder_path="${build_path%/*}"
 	echo $build_path
 	mkdir -p $folder_path
